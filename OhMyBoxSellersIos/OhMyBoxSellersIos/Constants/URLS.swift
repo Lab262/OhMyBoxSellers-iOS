790c4068da7579a0a2951fdbda23934fd48015a1
thiagotmb <tmb0710@gmail.com>
2016-11-18 02:58:22 -0200

    Adding starter project

---
+++ OhMyBoxSellersIos/OhMyBoxSellersIos/Constants/URLS.swift
@@ -0,0 +1,20 @@
+//
+//  URLS.swift
+//  OhMyBoxSellersIos
+//
+//  Created by Thiago-Bernardes on 18/11/16.
+//  Copyright © 2016 Lab262. All rights reserved.
+//
+
+import UIKit
+
+#if DEVELOPMENT
+let URL_WS_SERVER = "https://OhMyBoxSellersIos-stg.herokuapp.com/api/v0/"
+#else
+let URL_WS_SERVER = "https://OhMyBoxSellersIos-prod.herokuapp.com/api/v0/"
+#endif
+
+class URLS: NSObject {
+
+
+}
